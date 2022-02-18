75ad0582cdf2aec8396165462298bf22799747d4
lonelysamu <quahjit@gmail.com>
2022-02-19 05:04:59 +0800

    test

--- navbar.js
+++ navbar.js
@@ -3,6 +3,29 @@ document.querySelector("body").addEventListener('click', (e) => {
         document.getElementById("navbarcontent").classList.remove("d-none");
         document.getElementById("navbarcontent").classList.add("fadefromright");
     }
+    if (e.target.classList.contains("mainpagenavi") == true) {
+
+        y = Array.from(document.querySelector('#contentcontainer').getElementsByClassName("mainpagenavi")).indexOf(e.target);
+        document.querySelector(".navitemactive").classList.remove("navitemactive");
+        document.querySelectorAll(".navitem")[y+1].classList.add("navitemactive");
+        headerContent = document.getElementById("contentcontainer");
+        document.querySelector(".section").classList.remove("fadein");
+        document.querySelector(".section").classList.add("fadeout", "check");
+
+        document.querySelector('#contentcontainer').addEventListener("webkitAnimationEnd", (e) => {
+            if (e.target.classList.contains("section") == true) {
+                if (e.target.classList.contains("fadeout") == true) {
+                    if (e.target.classList.contains("check") == true) {
+                        e.target.classList.remove("check");
+                        text = "asset/template/" + Navbarvariable[y];
+                        fetchHtmlAsText(text)
+                            .then(data => headerContent.innerHTML = data);
+                    }
+                }
+            }
+        })
+    }
+
 })
 
 function onWKAnimEnd() {
