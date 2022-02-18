85f34291826a0f26374783e8b1f1c210d8282643
lonelysamu <quahjit@gmail.com>
2022-02-19 04:47:39 +0800

    navi

---
+++ shared.js
@@ -0,0 +1,13 @@
+
+Navbarvariable = ["home.html","about.html","livestage.html","competition.html"];
+
+
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+
+function removeall(item,index){
+    item.classList.add("d-none");
+    item.classList.remove("fadein");
+    item.classList.remove("fadefromleft2","hidetillscroll");}
