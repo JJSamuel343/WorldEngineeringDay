0a87cc5672c33f89db5de26075b654e76591cfa5
lonelysamu <quahjit@gmail.com>
2022-02-20 20:31:49 +0800

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -6,25 +6,22 @@ document.querySelector("body").addEventListener('click', (e) => {
     else if (e.target.classList.contains("mainpagenavi") == true) {
 
         z = Array.from(document.querySelector('#contentcontainer').getElementsByClassName("mainpagenavi")).indexOf(e.target);
-        console.log("mainpagenavi:" + (z+1));
         document.querySelector(".navitemactive").classList.remove("navitemactive");
-        document.querySelectorAll(".navitem")[z+1].classList.add("navitemactive");
+        document.querySelectorAll(".navitem")[z + 1].classList.add("navitemactive");
         headerContent = document.getElementById("contentcontainer");
         document.querySelector(".section").classList.remove("fadein");
         document.querySelector(".section").classList.add("fadeout", "check");
-
-        document.querySelector('#contentcontainer').addEventListener("webkitAnimationEnd", (e) => {
-            if (e.target.classList.contains("section") == true) {
-                if (e.target.classList.contains("fadeout") == true) {
-                    if (e.target.classList.contains("check") == true) {
-                        e.target.classList.remove("check");
-                        text = "asset/template/" + Navbarvariable[z+1];
-                        fetchHtmlAsText(text)
-                            .then(data => headerContent.innerHTML = data);
-                    }
-                }
-            }
-        })
+        document.querySelector('#contentcontainer').addEventListener('webkitAnimationEnd', ContentAnimationEnd);
     }
 
 })
+
+function ContentAnimationEnd(item) {
+    console.log("mainpagenavi:" + (item + 1));
+    text = "asset/template/" + Navbarvariable[item + 1];
+    fetchHtmlAsText(text)
+        .then(data => headerContent.innerHTML = data);
+    document.querySelector('#navbarcontent').removeEventListener('webkitAnimationEnd', ContentAnimationEnd);
+}
+
+
