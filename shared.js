60d80d2a0969918307e118e1b9dbb6b67a525bbb
lonelysamu <quahjit@gmail.com>
2022-03-03 11:20:58 +0800

    removemodal

--- shared.js
+++
@@ -1,9 +0,0 @@
-
-Navbarvariable = ["home.html","about.html","mranti.html","livestage.html","competition.html"];
-
-
-
-function removeall(item,index){
-    item.classList.add("d-none");
-    item.classList.remove("fadein");
-    item.classList.remove("fadefromleft2","hidetillscroll");}
