ad9592f0afe5752c9894420ff18b310420cca3af
lonelysamu <quahjit@gmail.com>
2022-03-03 16:18:55 +0800

    videos

--- modalCustom.js
+++ modalCustom.js
@@ -1,5 +1,5 @@
 
-let database = ["moRrIINP80M"]
+let database = ["p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","p4ArZyZ859Q","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo","BlbspOrcFEo"]
 
 document.querySelector('body').addEventListener('click', (e) => {
     if (e.target.classList.contains("loadmodalbutton") == true) {
@@ -11,7 +11,7 @@ document.querySelector('body').addEventListener('click', (e) => {
     else if(e.target.id == "closemodal" && (document.getElementById("videoframe").src !== "about:blank")){
         document.getElementById("videoframe").src = "about:blank";
     }
-    else if((e.target.id == "exampleModal") && (e.target.classname != "modal-header") && (e.target.classname != "modal-body") && (document.getElementById("videoframe").src !== "about:blank")){
+    else if((e.target.id == "exampleModal") || (e.target.classname != "modal-header") || (e.target.classname != "modal-body") || (document.getElementById("videoframe").src !== "about:blank")){
         document.getElementById("videoframe").src = "about:blank";
     }
     else{
