658d174bc9395b078b4376eb8e7bf2e33d5972e6
lonelysamu <quahjit@gmail.com>
2022-03-08 13:04:03 +0800

    newlanding

--- modalCustom1.js
+++ modalCustom1.js
@@ -1,11 +1,11 @@
 
-let database =["p4ArZyZ859Q?start=1595","p4ArZyZ859Q?start=22830","p4ArZyZ859Q?start=24949","BlbspOrcFEo?start=259","BlbspOrcFEo?start=19630"];
+let database =["BlbspOrcFEo?start=259"];
 
 document.querySelector('body').addEventListener('click', (e) => {
     if (e.target.classList.contains("loadmodalbutton") == true) {
         y = Array.from(document.querySelector('body').getElementsByClassName("loadmodalbutton")).indexOf(e.target);
         console.log(`Modal.js-onClick-index:${y}`,database[y]);
-        document.getElementById("videoframe").src = "https://www.youtube.com/embed/"+database[y];
+        document.getElementById("videoframe").src = "https://www.youtube.com/embed/BlbspOrcFEo?start=259"+database[y];
     }
 
     else if(e.target.id == "closemodal"){
